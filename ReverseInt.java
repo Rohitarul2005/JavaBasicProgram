53c0d0530c07c97d5d0850bcdafde3b883d3824e
Rohitarul2005 <rohitarulofficial@gmail.com>
2025-02-12 22:12:34 +0530

    Update ReverseInt.java

--- ReverseInt.java
+++ ReverseInt.java
@@ -4,9 +4,9 @@ class ReverseInt{
     int reverse=0;
     while(N>0)
     {
-      int r = N%10;
+      int rem= N%10;
        N =N/10;
-       reverse=reverse*10+r;
+       reverse=reverse*10+rem;
     }System.out.println(reverse);
    }
 }
